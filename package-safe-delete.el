28b931715bac00558668ca58f01ab4875f350e25
Fanael Linithien <fanael4@gmail.com>
2014-10-21 19:11:48 +0200

    Release version 0.1.4.

--- package-safe-delete.el
+++ package-safe-delete.el
@@ -2,7 +2,7 @@
 
 ;; Author: Fanael Linithien <fanael4@gmail.com>
 ;; URL: https://github.com/Fanael/package-safe-delete
-;; Version: 0.1.3
+;; Version: 0.1.4
 ;; Package-Requires: ((emacs "24.3") (epl "0.7-cvs"))
 
 ;; This file is NOT part of GNU Emacs.
